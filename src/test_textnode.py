616859d3ad65c5fca969c7c64982f40154214882
tokleinGit <toklein145@gmail.com>
2025-11-17 19:49:08 +0000

    minimalized

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -66,6 +66,80 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.props, {"src=": "url/of/image.jpg",
                                            "alt=": "Description of image"})
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_code(self):
+        node = TextNode(
+            "This is text with a `code block` word",
+            TextType.TEXT
+        )
+        new_nodes = split_nodes_delimiter([node], '`', TextType.CODE)
+        test_nodes = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+
+        def nodes_equal(node1, node2):
+            return (node1.text == node2.text and
+                    node1.text_type == node2.text_type and
+                    node1.url == node2.url)
+
+        # Im Test:
+        for i in range(len(test_nodes)):
+            self.assertTrue(
+                nodes_equal(new_nodes[i], test_nodes[i]),
+                f"Nodes not equal: {new_nodes[i]} != {test_nodes[i]}"
+            )
+
+class TestExtractImage(unittest.TestCase):
+    def test_extract_image(self):
+        text_img_1 = "![picture description](url/to/image.png)" 
+        text_img_2 = "![Bildbeschreibung](./relativ.png)"
+        text_cat = text_img_1 + " lorem ipsum " + text_img_2
+        self.assertEqual(
+            extract_markdown_images(text_img_1),
+            [("picture description", "url/to/image.png")]
+        )
+        
+        self.assertEqual(
+            extract_markdown_images(text_img_2),
+            [("Bildbeschreibung", "./relativ.png")]
+        )
+       
+        self.assertEqual(
+            len(extract_markdown_images(text_cat)),
+            len([text_img_1, text_img_2])
+        )
+
+        self.assertEqual(extract_markdown_links(text_img_1), [])
+        self.assertEqual(extract_markdown_links(text_img_2), [])
+        self.assertEqual(extract_markdown_links(text_cat), [])
+
+class TestExtractLink(unittest.TestCase):
+    def test_extract_link(self):
+        text_link_1 = "[alt text](https://www.google.com)"
+        text_link_2 = "[link](http://www.unsicher.de)"
+        text_cat = text_link_1 + " lorem ipsum " + text_link_2
+
+        self.assertEqual(
+            extract_markdown_links(text_link_1), 
+            [("alt text", "https://www.google.com")])
+
+        self.assertEqual(
+            extract_markdown_links(text_link_2),
+            [("link", "http://www.unsicher.de")]
+        )
+
+        self.assertEqual(
+            len(extract_markdown_links(text_cat)),
+            len([text_link_1, text_link_2])
+        )
+        
+        self.assertEqual(extract_markdown_images(text_link_1), [])
+        self.assertEqual(extract_markdown_images(text_link_2), [])
+        self.assertEqual(extract_markdown_images(text_cat), [])
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+import re
 from enum import Enum
 from htmlnode import LeafNode
 
@@ -12,7 +13,7 @@ class TextNode():
     def __eq__(self, node):
         if isinstance(node, TextNode):
             return False
-        
+            
         return (
             self.text == node.text and
             self.text_type.value == node.text_type.value and
@@ -56,3 +57,42 @@ def text_node_to_html_node(text_node):
             raise Exception("Invalid TextType for conversion!")
    
     return LeafNode(tag, value, props)
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        text = node.text
+
+        if text.count(delimiter) % 2 != 0:
+            raise Exception("Invalid Markdown syntax!")
+
+        while True:
+            if not delimiter in text:
+                break;
+            parts = text.split(delimiter, 2)
+            
+            if parts[0] == "":
+                new_nodes.append(TextNode(parts[1], text_type))
+            else:
+                new_nodes.extend(
+                    [
+                        TextNode(parts[0], TextType.TEXT),
+                        TextNode(parts[1], text_type),
+                    ]
+                )
+            text = parts[-1]
+
+        if text != "" and text != getattr(new_nodes[-1], "text"):
+            new_nodes.append(TextNode(text, TextType.TEXT))
+
+    return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
